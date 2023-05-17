fe097f1bce0fb3b2748d22638d33d08071e1c88b
AlexMaitland <alex.k.maitland@gmail.com>
2023-05-17 08:53:19 +0000

    Add functions to read the list of words from words.txt and start the game. We ask the user to input y or n if they wish to play - anything else will result in an error message

--- run.py
+++ run.py
@@ -1,3 +1,31 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
+
+def read_word_list(file_path):
+    with open(file_path, 'r') as file:
+        word_list = file.read().splitlines()
+    return word_list
+
+def start_game():
+    print("Welcome to the Guess 5 game!")
+    play = input("Would you like to play? (Y/N): ")
+    
+    if play.upper() == 'Y':
+        print("Great! Let's start the game.")
+        # Call the function or write code to start the game
+    elif play.upper() == 'N':
+        print("No problem. Maybe next time!")
+    else:
+        print("Invalid input. Please enter Y or N.")
+
+
+
+
+
+file_path = 'words.txt'  
+word_list = read_word_list(file_path)
+random_word = random.choice(word_list)
+print(word_list)
+print(random_word)
+
+
+start_game()
