eb3baab0538087dd27f9faeed8a07b89daf2f9df
AlexMaitland <alex.k.maitland@gmail.com>
2023-05-17 10:35:52 +0000

    Add function for selecting random word and clear up the read_word_list function

--- run.py
+++ run.py
@@ -1,9 +1,15 @@
 import random
 
-def read_word_list(file_path):
-    with open(file_path, 'r') as file:
-        word_list = file.read().splitlines()
+FILE_PATH = 'words.txt'
+
+def read_word_list():
+    f = open(FILE_PATH, 'r')
+    word_list = f.read().splitlines()
+    f.close()
     return word_list
+
+def select_word(word_list):
+    return random.choice(word_list)
 
 def start_game():
     print("Welcome to the Guess 5 game!")
@@ -21,9 +27,8 @@ def start_game():
 
 
 
-file_path = 'words.txt'  
-word_list = read_word_list(file_path)
-random_word = random.choice(word_list)
+word_list = read_word_list()
+random_word = select_word(word_list)
 print(word_list)
 print(random_word)
 
